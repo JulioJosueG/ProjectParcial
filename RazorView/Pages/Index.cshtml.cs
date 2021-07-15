000774229d8b1080e31405e106bc6acc9e9cd72c
JulioJosueG <jh2019-0417@uce.edu.do>
2021-07-15 19:55:56 -0400

    Intento de filtro(categorias,etc)

--- RazorView/Pages/Index.cshtml.cs
+++ RazorView/Pages/Index.cshtml.cs
@@ -20,6 +20,13 @@ namespace RazorView.Pages
         private readonly ILogger<IndexModel> _logger;
 
         public IList<Article> Articles { get; set; }
+        
+
+        public List<SelectListItem> OptionsCa{ get; set; }
+        public List<SelectListItem> OptionsCo { get; set; }
+        public List<SelectListItem> OptionsAu { get; set; }
+        public List<SelectListItem> OptionsSo { get; set; }
+
 
         public IndexModel(
             NewsProjectDBContext context,
@@ -29,40 +36,79 @@ namespace RazorView.Pages
             _logger = logger;
         }
 
-        public int[] arreglo = { 1, 2, 3, 4 };
-        
 
-        public async Task  OnGetAsync(string Search = null)
+        private void Views()
         {
-          
+            /* categories =  _context.Categories.Include(p => p.IdStateNavigation).Where(p => p.IdState == 1).ToList();
+            countries = _context.Countries.Include(p => p.IdStateNavigation).Where(p => p.IdState == 1).ToList();
+            sources = _context.Sources.Include(p => p.IdStateNavigation).Where(p => p.IdState == 1).ToList();
+            authors = _context.Authors.Include(p => p.IdStateNavigation).Include(p=> p.IdCountryNavigation).Where(p => p.IdState == 1).ToList();*/
+            OptionsCa = _context.Categories.Select(a =>
+                                 new SelectListItem
+                                 {
+                                     Value = a.IdCategory.ToString(),
+                                     Text = a.CategoryName
+                                 }).ToList();
+
+            OptionsCo = _context.Countries.Select(a =>
+                                             new SelectListItem
+                                             {
+                                                 Value = a.Code.ToString(),
+                                                 Text = a.CountryName
+                                             }).ToList(); 
+            OptionsAu = _context.Authors.Select(a =>
+                                             new SelectListItem
+                                             {
+                                                 Value = a.IdAuthor.ToString(),
+                                                 Text = a.Name
+                                             }).ToList(); 
+            OptionsSo = _context.Sources.Select(a =>
+                                             new SelectListItem
+                                             {
+                                                 Value = a.IdSource.ToString(),
+                                                 Text = a.SourceName
+                                             }).ToList();
+        }
+
+        public async Task  OnGetAsync(string Search , int? idCategoria, int? idContry, int? idAuthor, int? idSource)
+        {
+            Views();
+            
+            if (idCategoria != null)
+            {
+                Articles = await _context.Articles.Include(p => p.CountryCodeNavigation).Include(p => p.IdAuthorNavigation).
+                    Include(p => p.IdSourceNavigation).Include(p => p.IdStateNavigation).Where(p => p.CategoryId == idCategoria & p.IdState == 1).ToListAsync();
+            }
+            else if (idContry != null)
+            {
+                Articles = await _context.Articles.Include(p => p.CountryCodeNavigation).Include(p => p.IdAuthorNavigation).
+                    Include(p => p.IdSourceNavigation).Include(p => p.IdStateNavigation).Where(p => p.CategoryId == idContry & p.IdState == 1).ToListAsync();
+            }
+            else if (idAuthor != null)
+            {
+                Articles = await _context.Articles.Include(p => p.CountryCodeNavigation).Include(p => p.IdAuthorNavigation).
+                    Include(p => p.IdSourceNavigation).Include(p => p.IdStateNavigation).Where(p => p.CategoryId == idAuthor & p.IdState == 1).ToListAsync();
+            }
+            else if (idSource != null)
+            {
+                Articles = await _context.Articles.Include(p => p.CountryCodeNavigation).Include(p => p.IdAuthorNavigation).
+                    Include(p => p.IdSourceNavigation).Include(p => p.IdStateNavigation).Where(p => p.CategoryId == idSource & p.IdState == 1).ToListAsync();
+            }
+
             if (string.IsNullOrEmpty(Search))
             {
                 Articles = await _context.Articles.Include(p => p.CountryCodeNavigation).Include(p => p.IdAuthorNavigation).
-                    Include(p => p.IdSourceNavigation).Include(p => p.IdStateNavigation).Where(p => p.IdState == 1) .ToListAsync();
+                    Include(p => p.IdSourceNavigation).Include(p => p.IdStateNavigation).Where(p => p.IdState == 1).ToListAsync();
             }
             else
             {
                 Articles = await _context.Articles.Include(p => p.CountryCodeNavigation).Include(p => p.IdAuthorNavigation).
-                    Include(p => p.IdSourceNavigation).Include(p => p.IdStateNavigation).Where(p => p.Title.Contains(Search) & p.IdState ==1).ToListAsync()
+                    Include(p => p.IdSourceNavigation).Include(p => p.IdStateNavigation).Where(p => p.Title.Contains(Search) & p.IdState == 1).ToListAsync();
 
-
-
-                    ;
             }
 
         }
 
-      
-
-        public IActionResult OnGetArticles()
-        {
-           
-            return new OkObjectResult(_context.Articles.Include(p => p.IdSourceNavigation).Include(p => p.IdAuthorNavigation).
-                Include(p => p.IdStateNavigation).Include(p => p.CountryCodeNavigation));
-
-        }
-
-        
 
     }
 }
